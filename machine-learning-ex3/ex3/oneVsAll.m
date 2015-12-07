482818b3a86ea1c1948c44c3d7abf9cc9eb274d7
liujun <liujun77@gmail.com>
2015-12-07 15:21:31 -0600

    ex3_1

--- machine-learning-ex3/ex3/oneVsAll.m
+++ machine-learning-ex3/ex3/oneVsAll.m
@@ -50,7 +50,16 @@ X = [ones(m, 1) X];
 %
 
 
+for i=1:num_labels
 
+    initial_theta = zeros(n + 1, 1);
+    options = optimset('GradObj', 'on', 'MaxIter', 50);
+    [all_theta(i,:)] = ...
+        fmincg (@(J)(lrCostFunction(J, X, (y == i), lambda)), ...
+        initial_theta, options);
+
+
+end
 
 
 
